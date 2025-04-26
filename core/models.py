bf047f5e3e9768727f74b810e6280f73705c55f5
C0159412020 <adlightakinyi1@gmail.com>
2025-04-26 22:50:30 +0300

    defined the Client, Program, and Enrollment models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Program(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=200)
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.full_name
+
+class Enrollment(models.Model):
+    client = models.ForeignKey(Client, related_name="enrollments", on_delete=models.CASCADE)
+    program = models.ForeignKey(Program, related_name="enrollments", on_delete=models.CASCADE)
+    date_enrolled = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.client.full_name} - {self.program.name}"
